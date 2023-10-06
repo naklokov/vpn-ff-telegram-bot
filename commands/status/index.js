168d0979fc564953ccf0e7208792b3a60381bb96
naklokov <bib0@mail.ru>
2023-10-06 20:51:02 +0300

    fix: Ошибка незарегистрированного пользователя

--- commands/status/index.js
+++ commands/status/index.js
@@ -7,16 +7,14 @@ module.exports = async (ctx) => {
   const { id: chatId } = getUserPersonalDataFromContext(ctx);
   const user = await usersConnector.getUserByChatId(chatId);
 
-  if (!user.chatId) {
+  if (!user?.chatId) {
     ctx.reply(
-      `Вы пока что не зарегистрированы в системе, пройдите пожалуйста регистрацию 👉 /${CMD.registration}`
+      `Вы пока что не зарегистрированы в системе, пройдите регистрацию 👉 /${CMD.registration}`
     );
     return;
   }
 
-  const statusText = `Статус подключения: ${
-    user.isActive ? "🟢 Активен" : "🔴 Не активен"
-  }`;
+  const statusText = user.isActive ? "🟢 Активен" : "🔴 Не активен";
   const expiredDateText = `Дата окончания оплаченного периода: ${dayjs(
     user.expiredDate
   ).format("DD.MM.YYYY")}`;
