37721ae10a7ee5eb80d9fea54146ba4f58b6a5a6
Николай Клоков <bib0@mail.ru>
2026-04-08 02:14:53 +0300

    feature: Добавить статьи в help

--- commands/help/index.js
+++ commands/help/index.js
@@ -7,15 +7,30 @@ module.exports = async (ctx) => {
   const { id: chatId } = getUserPersonalDataFromContext(ctx);
   const dbUser = await usersConnector.getUserByChatId(chatId);
 
-  await ctx.reply(
-    `Если у вас возникли вопросы, пишите 👉 ${DEVELOPER_CONTACT}`,
-    exitButton,
-  );
-
   if (dbUser) {
+    await ctx.reply(
+      `Если у вас возникли проблемы с подключением VPN, то посмотрите информацию из статьи ниже:\nhttps://naklokov.yonote.ru/doc/ne-rabotaet-vpn-lgXCDKbWlQ`,
+    );
+
+    await ctx.reply(
+      `\nЕсли вы оплатили подписку, но не можете подключиться, то посмотрите информацию из статьи ниже:\nhttps://naklokov.yonote.ru/doc/podpiska-okonchilas-zneDyM5lGo#b_91853_4cfea05a6980`,
+    );
+
+    await ctx.reply(
+      `\nЕсли ваша проблема не решена, напишите нам 👉 ${DEVELOPER_CONTACT}`,
+      exitButton,
+    );
+
     await ctx.reply(
       `Для оперативного решения проблемы сразу укажите ваш логин \`${dbUser.phone}\` и на каком устройстве вы используете ВПН`,
       { parse_mode: "MarkdownV2" },
     );
+
+    return;
   }
+
+  await ctx.reply(
+    `Если у вас возникли вопросы, пишите 👉 ${DEVELOPER_CONTACT}`,
+    exitButton,
+  );
 };
